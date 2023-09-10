184003e6d23cd9d694ebe4c526a8a9df3fb22291
Naveen <practialengineers05@gmail.com>
2023-09-10 22:19:09 +0530

    angular setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './Component/home-page/home-page.component';
+import { NavBarComponent } from './Shared/nav-bar/nav-bar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { ProjectCardsComponent } from './Shared/project-cards/project-cards.component';
+import {MatCardModule} from '@angular/material/card'
+import { HttpClientModule } from  '@angular/common/http';
+import { QuestionPageComponent } from './Component/question-page/question-page.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    ProjectCardsComponent,
+    QuestionPageComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule, 
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
