50d77ba1f93ce2e6a309632a9d6361ea4b1ff311
Naveen <practialengineers05@gmail.com>
2023-09-10 22:20:09 +0530

    project service and model

---
+++ src/app/Models/project.ts
@@ -0,0 +1,5 @@
+export interface Project {
+    id: String,
+    projectName : String,
+    image : String
+  }
---
+++ src/app/Service/Shared/project-cards/project-card.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import { Project } from 'src/app/Models/project';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectCardService {
+  
+
+  private url = "http://localhost:8080/project/list";
+
+  constructor(private http : HttpClient) { }
+
+
+  getAllProjects(){
+    return this.http.get<Project[]>(this.url);
+  }
+
+}
