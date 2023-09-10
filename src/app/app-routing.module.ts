6710ae37e737e2d76a9fec6e46fb465c82e9d24a
Naveen <practialengineers05@gmail.com>
2023-09-10 22:18:02 +0530

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './Component/home-page/home-page.component';
+import { QuestionPageComponent } from './Component/question-page/question-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'project/:id',component:QuestionPageComponent},
+  { path:'',component:HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
