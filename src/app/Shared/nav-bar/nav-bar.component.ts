5310446c3ea2723bf66e237b9cfb0992bb55ecbb
engineerscodes <practicalengineers05@gmail.com>
2023-10-08 19:39:12 +0530

    Navbar added

--- src/app/Shared/nav-bar/nav-bar.component.ts
+++ src/app/Shared/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { NavbarService } from 'src/app/Service/navbar.service';
 
 
 @Component({
@@ -6,6 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit,AfterViewInit{
+  
+  constructor( private navBarService : NavbarService ){}
+
+    
+  @ViewChild('matNavbar', { static: true })
+  matNavbar!: ElementRef ;
+
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    var width = this.matNavbar.nativeElement.offsetWidth;
+    this.navBarService.heightOfNavbar = this.matNavbar.nativeElement.offsetHeight;
+    console.log('Width:' + width);
+    console.log('Height: ' + this.navBarService.heightOfNavbar);
+  }
 
 }
