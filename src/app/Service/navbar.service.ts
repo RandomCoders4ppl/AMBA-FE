402956f7193f02723ff613c8b611529c50cd88e7
engineerscodes <practicalengineers05@gmail.com>
2023-10-08 19:37:27 +0530

    Nabar sevice for height

---
+++ src/app/Service/navbar.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavbarService {
+
+  constructor() { }
+  
+  public  heightOfNavbar : number = 64;
+
+}
