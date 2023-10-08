74657220f2e5a32098111f3865e2e7da4950f38e
engineerscodes <practicalengineers05@gmail.com>
2023-10-08 19:37:50 +0530

    project and types service

---
+++ src/app/Service/project-types.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProjectType } from '../Models/project_type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectTypesService {
+
+  TYPES_BASE_URL = "http://localhost:8080/projects/types"
+
+  constructor(private http : HttpClient) { }
+
+  getAllTypes():Observable<any>{
+    const URL = this.TYPES_BASE_URL + '/list'
+    return this.http.get<ProjectType>(URL);
+  }
+  
+  saveNewProjectType(pType  : String):Observable<any>{
+    const URL = this.TYPES_BASE_URL + "/new"
+    return this.http.post<any>(URL,pType);
+  }
+
+}
