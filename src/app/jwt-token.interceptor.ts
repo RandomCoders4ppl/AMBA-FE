bf9a05cbe53d4d1ea79b5c7551b3e18b0d6ebdb3
engineerscodes <practicalengineers05@gmail.com>
2023-10-18 22:54:24 +0530

    redirect to login

--- src/app/jwt-token.interceptor.ts
+++ src/app/jwt-token.interceptor.ts
@@ -7,16 +7,20 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './Service/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class JwtTokenInterceptor implements HttpInterceptor {
 
-  constructor(private JwtAuth : AuthService) {}
+  constructor(private JwtAuth : AuthService,private router: Router) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
   
     try{
     const JwtToken = this.JwtAuth.getJwtToken();
+    if(this.JwtAuth.getExpirationDate()){
+      this.router.navigate(["/login"]);
+    }
     
     const JwtReq = request.clone({setHeaders: { 'Authorization': `Bearer ${JwtToken}`}})
     console.log(JwtReq)
