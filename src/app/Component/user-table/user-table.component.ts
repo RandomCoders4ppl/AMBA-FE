bd89785aa4ecd833f8efc0e4482ed4dd481a25ee
engineerscodes <practicalengineers05@gmail.com>
2023-10-28 23:57:21 +0530

    table added

---
+++ src/app/Component/user-table/user-table.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit } from '@angular/core';
+import { HotTableRegisterer } from '@handsontable/angular';
+import Handsontable from 'handsontable';
+import { ReportService } from 'src/app/Service/report.service';
+import { Report } from '../../Models/report';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-user-table',
+  templateUrl: './user-table.component.html',
+  styleUrls: ['./user-table.component.css']
+})
+export class UserTableComponent implements OnInit {
+
+  hotRegisterer = new HotTableRegisterer();
+  id = 'hotInstance';
+  data : Report[] = []
+
+  hotSettings: Handsontable.GridSettings = {
+    data: this.data,
+    columns: [
+      {
+        title: 'Email',
+        type: 'text',
+        data: 'email',
+        readOnly: true
+      },
+      {
+        title: 'Project',
+        type: 'text',
+        data: 'project',
+        readOnly: true
+      },
+      {
+        title: 'Project Type',
+        type: 'text',
+        data: 'type',
+        readOnly: true
+      },
+      {
+        title: 'Question completed',
+        type: 'numeric',
+        data: 'questionNumber',
+        readOnly: true
+      },
+      {
+        title: 'Score',
+        type: 'text',
+        data: 'score',
+        className: 'htRight',
+        readOnly: true
+      },
+      {
+        title: 'Total Questions',
+        type: 'text',
+        data: 'totalQuestions',
+        className: 'htCenter',
+        readOnly: true
+      },
+      {
+        title: 'Role',
+        type: 'text',
+        data: 'role',
+        className: 'htRight',
+        readOnly: true
+      },
+      {
+        title: 'Change Role',
+        type: 'checkbox',
+        data: 'inStock',
+        className: 'htCenter',
+      },
+      {
+        title: 'View All Answer',
+        type: 'text',
+        data: 'model',
+      },
+      {
+        title: 'Report Date and Time',
+        type: 'text',
+        data: 'reportDateTime',
+        readOnly: true
+      }
+    ],
+    colHeaders: true,
+    filters: true,
+    dropdownMenu: true,
+    height: 'auto',
+    licenseKey: 'non-commercial-and-evaluation',
+
+  };
+
+  constructor(private reportService: ReportService) { }
+
+  ngOnInit(): void {
+    this.reportService.getAdminReport().subscribe(res => { 
+      let hot = this.hotRegisterer.getInstance(this.id);
+      res.forEach((element :Report) => {
+        element.reportDateTime = moment(element.reportDateTime).format('DD/MM/YYYY HH:MM:SS');
+        this.data.push(element)
+      });
+      hot.loadData(this.data)
+      hot.render()
+      })
+  }
+
+}
