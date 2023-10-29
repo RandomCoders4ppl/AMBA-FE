d0794d1cb06b8f5dc93f7bc8868fc3cbd25c5b1d
engineerscodes <practicalengineers05@gmail.com>
2023-10-29 23:22:11 +0530

    new model

---
+++ src/app/Models/AdminReqType.ts
@@ -0,0 +1,4 @@
+export interface AdminReqType  {
+    userUuid: string, email: string;
+}
+  
---
+++ src/app/Models/Users.ts
@@ -0,0 +1,6 @@
+export interface Users {
+    userUuid : string ,
+    email : string,
+    name : string,
+    role?:string
+  }
