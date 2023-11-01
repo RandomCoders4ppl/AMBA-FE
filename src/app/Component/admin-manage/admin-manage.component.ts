1862a6fe8925eeffe319f49883bfd636523a1bef
engineerscodes <practicalengineers05@gmail.com>
2023-11-01 14:38:19 +0530

    Onchange method

--- src/app/Component/admin-manage/admin-manage.component.ts
+++ src/app/Component/admin-manage/admin-manage.component.ts
@@ -68,12 +68,12 @@ export class AdminManageComponent implements AfterViewInit, OnDestroy {
     },
     licenseKey: 'non-commercial-and-evaluation',
     afterChange: (changes, src) => {
-       if(changes)
-       this.onChangeValue(changes,src)
+      if (changes)
+        this.onChangeValue(changes, src)
     }
   };
 
-  constructor(private adminTool: AdminToolService,private authService : AuthService) { }
+  constructor(private adminTool: AdminToolService, private authService: AuthService) { }
 
   ngAfterViewInit(): void {
     let hot = this.hotRegisterer.getInstance(this.mt_id);
@@ -103,19 +103,19 @@ export class AdminManageComponent implements AfterViewInit, OnDestroy {
     if (source === 'edit')
       for (let i = 0; i < changes.length; i++) {
         const [row, prop, oldVal, newVal] = changes[i];
-        if(prop==='changeRole' || prop ==='deleteUser'){
-           const email : string = instance.getDataAtCell(row,instance.propToCol('email'))
-           const loggedInUser: string = this.authService.getEmail()
-           const value1 = instance.getDataAtCell(row,instance.propToCol('changeRole'))
-           const value2 = instance.getDataAtCell(row,instance.propToCol('deleteUser'))
-           if(value1==true && value2==true){
+        if (prop === 'changeRole' || prop === 'deleteUser') {
+          const email: string = instance.getDataAtCell(row, instance.propToCol('email'))
+          const loggedInUser: string = this.authService.getEmail()
+          const value1 = instance.getDataAtCell(row, instance.propToCol('changeRole'))
+          const value2 = instance.getDataAtCell(row, instance.propToCol('deleteUser'))
+          if (value1 == true && value2 == true) {
             alert("Not possible to delete user and Same Time make him Admin")
-            instance.setDataAtCell(row,instance.propToCol(prop),oldVal)
-           }
-           if(email!=null && loggedInUser !=null && loggedInUser.toLocaleLowerCase() === email.toLocaleLowerCase() && newVal == true){
+            instance.setDataAtCell(row, instance.propToCol(prop), oldVal)
+          }
+          if (email != null && loggedInUser != null && loggedInUser.toLocaleLowerCase() === email.toLocaleLowerCase() && newVal == true) {
             alert("Can't change your Own Role or Delete yourself from here ....")
-            instance.setDataAtCell(row,instance.propToCol(prop),oldVal)
-           }
+            instance.setDataAtCell(row, instance.propToCol(prop), oldVal)
+          }
         }
       }
 
@@ -124,35 +124,65 @@ export class AdminManageComponent implements AfterViewInit, OnDestroy {
   deleteUsers() {
     const instance = this.hotRegisterer.getInstance(this.mt_id);
     const data = instance.getDataAtCol(instance.propToCol('deleteUser'))
-    let deleteUsersList: { userUuid: string; email: string; }[] = []
-    data.forEach((ele,index)=>{
-      if(ele == true){
-      const userID = instance.getDataAtCell(index,instance.propToCol('userUuid'))
-      const Email = instance.getDataAtCell(index,instance.propToCol('email'))
-      deleteUsersList.push({'userUuid':userID,'email':Email})
+    let deleteUsersList: AdminReqType[] = []
+    data.forEach((ele, index) => {
+      if (ele == true) {
+        const userID = instance.getDataAtCell(index, instance.propToCol('userUuid'))
+        const Email = instance.getDataAtCell(index, instance.propToCol('email'))
+        deleteUsersList.push({ 'userUuid': userID, 'email': Email })
       }
     })
-    console.log(deleteUsersList)
+    if (deleteUsersList.length > 0)
+      this.adminTool.postChanges("/deleteUsers", deleteUsersList).subscribe(res => {
+        res.forEach(ele => {
+          const UserUUID = instance.getDataAtProp('userUuid');
+          const index = UserUUID.findIndex(e => e === ele.userUuid)
+          if(ele.deleted && ele.deleted?.toLowerCase()==="true"){
+            this.ChangeRowColor(index,'RipUser')
+          }
+        })
+      })
   }
 
   onChangeRole() {
     const instance = this.hotRegisterer.getInstance(this.mt_id);
     const data = instance.getDataAtCol(instance.propToCol('changeRole'))
-    let changeRoleList : AdminReqType[] = []
-    data.forEach((ele,index)=>{
-      if(ele == true){
-      const userID = instance.getDataAtCell(index,instance.propToCol('userUuid'))
-      const Email = instance.getDataAtCell(index,instance.propToCol('email'))
-      changeRoleList.push({'userUuid':userID,'email':Email})
+    let changeRoleList: AdminReqType[] = []
+    data.forEach((ele, index) => {
+      if (ele == true) {
+        const userID = instance.getDataAtCell(index, instance.propToCol('userUuid'))
+        const Email = instance.getDataAtCell(index, instance.propToCol('email'))
+        changeRoleList.push({ 'userUuid': userID, 'email': Email })
       }
     })
-    if(changeRoleList.length>0)
-    this.adminTool.postChangeRoles(changeRoleList).subscribe(res => console.log(res))
+    if (changeRoleList.length > 0)
+      this.adminTool.postChanges('/ChangeRoles', changeRoleList).subscribe(res => {
+        res.forEach(ele => {
+          const UserIdCol = instance.getDataAtProp('userUuid');
+          const index = UserIdCol.findIndex(e => e === ele.userUuid)
+          if (ele.newRole && ele.newRole.trim().length > 0) {
+            instance.setDataAtCell(index, instance.propToCol('role'), ele.newRole)
+            this.ChangeRowColor(index,'ChangeRoleYes')
+          }else  this.ChangeRowColor(index,'ChangeRoleNo')
+        })
+      })
   }
 
 
   ngOnDestroy(): void {
     this.hotRegisterer.getInstance(this.mt_id).destroy();
+  }
+
+  ChangeRowColor(rowIndex: number, CssClassName: string) {
+    const instance = this.hotRegisterer.getInstance(this.mt_id);
+    for (let i = 0; i < instance.countCols(); i++) {
+      if(CssClassName === 'RipUser') {
+        instance.setDataAtCell(rowIndex, instance.propToCol('deleteUser'), false)
+        instance.setCellMeta(rowIndex,i, 'readOnly',true);
+      }
+      instance.setCellMeta(rowIndex, i, 'className', CssClassName);
+    }
+    instance.render()
   }
 
 
