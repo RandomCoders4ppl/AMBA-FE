540db6c74c5613a37fd45f2bca9ef7844c07d2a4
engineerscodes <practicalengineers05@gmail.com>
2023-11-04 22:03:48 +0530

    Pagenation added

--- src/app/Component/user-table/user-table.component.ts
+++ src/app/Component/user-table/user-table.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { HotTableRegisterer } from '@handsontable/angular';
 import Handsontable from 'handsontable';
 import { ReportService } from 'src/app/Service/report.service';
@@ -10,11 +10,14 @@ import * as moment from 'moment';
   templateUrl: './user-table.component.html',
   styleUrls: ['./user-table.component.css']
 })
-export class UserTableComponent implements AfterViewInit,OnDestroy {
+export class UserTableComponent implements AfterViewInit, OnDestroy {
 
   hotRegisterer = new HotTableRegisterer();
   id = 'hotInstance';
-  data : Report[] = []
+  data: Report[] = []
+
+  Page = 0;
+  MaxPage = 50;
 
   hotSettings: Handsontable.GridSettings = {
     data: this.data,
@@ -76,10 +79,21 @@ export class UserTableComponent implements AfterViewInit,OnDestroy {
         readOnly: true
       }
     ],
+    rowHeaders: true,
     colHeaders: true,
     filters: true,
     dropdownMenu: true,
-    height: 'auto',
+    rowHeights: 50,
+    height: 400,
+    stretchH: 'all',
+    fixedColumnsLeft: 3,
+    manualColumnResize: true,
+    manualRowResize: true,
+    contextMenu: true,
+    viewportColumnRenderingOffset: 10,
+    manualColumnMove: true,
+    manualRowMove: true,
+    autoWrapRow: true,
     licenseKey: 'non-commercial-and-evaluation',
 
   };
@@ -87,15 +101,37 @@ export class UserTableComponent implements AfterViewInit,OnDestroy {
   constructor(private reportService: ReportService) { }
 
   ngAfterViewInit(): void {
-    this.reportService.getAdminReport().subscribe(res => { 
-      let hot = this.hotRegisterer.getInstance(this.id);
-      res.forEach((element :Report) => {
+    let hot = this.hotRegisterer.getInstance(this.id);
+    this.reportService.getAdminReport().subscribe(res => {
+      res.content.forEach(element=> {
         element.reportDateTime = moment(element.reportDateTime).format('DD/MM/YYYY HH:MM:SS');
         this.data.push(element)
       });
+      this.MaxPage = res.totalPages ;
       hot.loadData(this.data)
       hot.render()
-      })
+      this.Page++;
+    })
+
+    hot.addHook('afterScrollVertically', () => {
+      const element = document.getElementsByClassName('wtHolder')[0]
+      if (element) {
+        if (element.scrollTop + element.clientHeight >= element.scrollHeight -50 && this.Page < this.MaxPage) {
+          this.reportService.getAdminReport(this.Page).subscribe(res => {
+            res.content.forEach((element: Report) => {
+              element.reportDateTime = moment(element.reportDateTime).format('DD/MM/YYYY HH:MM:SS');
+              this.data.push(element)
+            });
+            this.MaxPage = res.totalPages ;
+            hot.updateData(this.data)
+            hot.render()
+            this.Page++;
+          })
+        }
+      }
+    }
+    )
+
   }
 
   ngOnDestroy() {
--- src/app/Service/report.service.ts
+++ src/app/Service/report.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Report } from '../Models/report';
+import { AdminReportPage } from '../Models/AdminReportPage';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,8 @@ export class ReportService {
 
   constructor(private http : HttpClient) { }
   
-  getAdminReport(): Observable<any>{
-     return this.http.get<Report[]>(this.BASE_ADMIN_URL+"report");
+  getAdminReport(page = 0): Observable<AdminReportPage>{
+     return this.http.get<AdminReportPage>(this.BASE_ADMIN_URL+"report?page="+page);
   }
 
 }
