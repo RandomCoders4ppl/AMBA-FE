8c27e5a01b642d2d2f9e9d7dea9b3f52686b45c0
engineerscodes <practicalengineers05@gmail.com>
2023-11-10 00:18:02 +0530

    handle 200 post & post

---
+++ src/app/successpost.inerceptor.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+  HttpResponse
+} from '@angular/common/http';
+import { Observable, catchError, tap, throwError } from 'rxjs';
+import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from '@angular/material/snack-bar';
+
+@Injectable()
+export class PostSuccessInterceptor implements HttpInterceptor {
+
+  constructor(private snackBar: MatSnackBar) {}
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'start';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+
+  intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(req).pipe(
+        tap(event => {
+          if (event instanceof HttpResponse && req.method === 'POST' || req.method === 'PUT') {
+            this.snackBar.open('Successful POST','close',{
+                duration: 5000,
+                horizontalPosition: this.horizontalPosition,
+                verticalPosition: this.verticalPosition,
+                panelClass:['my-custom-snackbar2'],
+              });
+          }
+        })
+      );
+  }
+}
