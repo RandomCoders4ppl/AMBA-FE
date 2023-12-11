7ec1c598de2f35edd3b9d759be1ea439e8629f4e
AmishaShaw <Amisha.Shaw@BakerHughes.com>
2023-12-11 14:07:48 +0530

    Revert "Merge pull request #5 from engineerscodes/master-shawami"
    This reverts commit f2209b8bcc55b5807ba0a2fdfb77b8677dba6990, reversing
    changes made to c0bc6fb99279ac424768e27a3c291a85d08bb03a.

--- src/app/Component/admin-manage/admin-manage.component.ts
+++ src/app/Component/admin-manage/admin-manage.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
-import { HotTableRegisterer,HotTableModule } from '@handsontable/angular';
+import { HotTableRegisterer } from '@handsontable/angular';
 import Handsontable from 'handsontable';
 import { CellChange, ChangeSource } from 'handsontable/common';
 import { error } from 'handsontable/helpers';
