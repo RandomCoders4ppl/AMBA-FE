ea6f6c007c5b357cd436ef37328acf314f0d3353
engineerscodes <practicalengineers05@gmail.com>
2023-12-14 23:29:36 +0530

    hide footer when route is project

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+
   aboutUS = [{text :"AMBA NGO",link:"https://www.ambaforlife.org/"}]
   US = [{text :"AMBA NGO",link:"https://www.ambaforlife.org/"}]
   Projects = [{text :"AMBA NGO",link:"https://www.ambaforlife.org/"}]
   date = new Date().getFullYear()
+
+  hide :boolean=false;
+
+  constructor(private router:Router){}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        if(event.url.includes("/project/")){
+          this.hide=true;
+        }else{
+          this.hide=false;
+        }
+      }
+    });
+  }
+
 }
