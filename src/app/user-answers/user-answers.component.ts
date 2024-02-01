d0e8ededf609dec8d240c0d227e0564ab07fde16
engineerscodes <practicalengineers05@gmail.com>
2024-02-01 22:37:31 +0530

    new component

---
+++ src/app/user-answers/user-answers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-answers',
+  templateUrl: './user-answers.component.html',
+  styleUrls: ['./user-answers.component.css']
+})
+export class UserAnswersComponent {
+
+}
