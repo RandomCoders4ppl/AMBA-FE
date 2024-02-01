eaf89290e288698a9d45d7ccae0cfd2ac9655af9
engineerscodes <practicalengineers05@gmail.com>
2024-02-01 22:36:40 +0530

    new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,14 @@ import { RoleGuard} from './Ngguard/guard.guard';
 import { AdminManageComponent } from './Component/admin-manage/admin-manage.component';
 import { ProfileComponent } from './Component/profile/profile.component';
 import { UserTableComponent } from './Component/user-table/user-table.component';
+import { UserAnswersComponent } from './user-answers/user-answers.component';
 
 const routes: Routes = [
   {path:'project/:id',component:QuestionPageComponent,canActivate:[RoleGuard]},
   {path:'admin/questions',component:AdminQuestionPageComponent,canActivate:[RoleGuard]},
   {path:"admin/managetool",component:AdminManageComponent,canActivate:[RoleGuard]},
   {path:"admin/report",component:UserTableComponent,canActivate:[RoleGuard]},
+  {path:'admin/useranswers/:projectid/:email',component:UserAnswersComponent,canActivate:[RoleGuard]},
   {path:'admin',component:AdminPageComponent,canActivate:[RoleGuard]},
   {path:'login',component:AuthComponent},
   {path:'',component:HomePageComponent,canActivate:[RoleGuard]},
